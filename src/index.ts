05fb269958cff4cddb36147b7922baffbfd594e5
Thanaphat Ketsani <65160169@go.buu.ac.th>
2024-06-08 17:50:34 +0700

    database setup

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,21 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
+
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRRING as string)
+  .then(() => console.log("Connect to database."));
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.get("/test", async (req:Request, res:Response) => {
-    res.json({ message: "Hello" });
+app.get("/test", async (req: Request, res: Response) => {
+  res.json({ message: "Hello" });
 });
 
 app.listen(7000, () => {
-    console.log("server started on localhost:7000");
+  console.log("server started on localhost:7000");
 });
