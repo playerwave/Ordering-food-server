66be3b653a8edb0d570238047e6903d5627ffd6d
Thanaphat Ketsani <65160169@go.buu.ac.th>
2024-06-18 07:10:50 +0700

    Update user profile api

--- src/routes/MyUserRoute.ts
+++ src/routes/MyUserRoute.ts
@@ -1,10 +1,17 @@
-import express from 'express';
-import MyUserController from '../controllers/MyUserController';
-import { jwtCheck } from '../middleware/auth';
+import express from "express";
+import MyUserController from "../controllers/MyUserController";
+import { jwtCheck, jwtParse } from "../middleware/auth";
+import { validationMyUserRequest } from "../middleware/validation";
 
 const router = express.Router();
 
-router.post("/", jwtCheck, MyUserController.createCurrentUser)
-
+router.post("/", jwtCheck, MyUserController.createCurrentUser);
+router.put(
+  "/",
+  jwtCheck,
+  jwtParse,
+  validationMyUserRequest,
+  MyUserController.updateCurrentUser
+);
 
 export default router;
